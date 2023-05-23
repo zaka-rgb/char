ca76889a11a0bfa9c33bbfb0aed003c087604a21
Zakaria Ben <75684862+zaka-rgb@users.noreply.github.com>
2023-05-23 23:10:49 +0200

    Add files via upload

---
+++ char/Untitled1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define NOMBRE -8
+#if (-1=0)
+
+#endif // x
+
+
+
+main()
+{
+
+    printf("%d",NOMBRE );
+}
---
+++ char/Untitled2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+main()
+{
+    printf("%s",__TIME__);
+
+}
